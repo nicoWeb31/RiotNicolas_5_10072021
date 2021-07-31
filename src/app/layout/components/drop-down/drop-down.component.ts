8de5a5b7a592eb1a68c7b9c571db2ce120dc2f81
Nicolas Riot <nriot@umoove-inc.com>
2021-07-31 10:41:28 +0200

    dropdown autre aproche

--- src/app/layout/components/drop-down/drop-down.component.ts
+++ src/app/layout/components/drop-down/drop-down.component.ts
@@ -1,4 +1,11 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  HostListener,
+  OnInit,
+  Output,
+} from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 // https://www.tutsmake.com/angular-11-select-dropdown-example-tutorial/
@@ -9,19 +16,43 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class DropDownComponent {
   @Output() emitFilter = new EventEmitter<string>();
-  constructor() {}
+  @HostListener('document:click', ['$event'])
+  clickout(event: any) {
+    if (this.eRef.nativeElement.contains(event.target)) {
+      console.log('inside');
+    } else {
+      console.log('out');
+    }
+  }
 
+  constructor(private eRef: ElementRef) {}
 
   form = new FormGroup({
     filters: new FormControl('', Validators.required),
   });
 
   getFormValue() {
-    console.log('pop')
-
+    console.log('pop');
   }
 
   setValue(value: any) {
     this.emitFilter.emit(value);
   }
+
+  operDropdown() {
+    document.getElementById('myDropdown')?.classList.toggle('show');
+  }
+
+  // window.onclick = function(event) {
+  //   if (!event.target.matches('.dropbtn')) {
+  //     var dropdowns = document.getElementsByClassName("dropdown-content");
+  //     var i;
+  //     for (i = 0; i < dropdowns.length; i++) {
+  //       var openDropdown = dropdowns[i];
+  //       if (openDropdown.classList.contains('show')) {
+  //         openDropdown.classList.remove('show');
+  //       }
+  //     }
+  //   }
+  // }
 }
