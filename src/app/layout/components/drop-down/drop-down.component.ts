4e8f4a8ae0ae60cd0a7ab9efe65cfc9daada3408
Nicolas Riot <nriot@umoove-inc.com>
2021-08-03 19:45:36 +0200

    modif dropdown

--- src/app/layout/components/drop-down/drop-down.component.ts
+++ src/app/layout/components/drop-down/drop-down.component.ts
@@ -1,12 +1,5 @@
-import {
-  Component,
-  ElementRef,
-  EventEmitter,
-  HostListener,
-  OnInit,
-  Output,
-} from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Observable } from 'rxjs';
 
 // https://www.tutsmake.com/angular-11-select-dropdown-example-tutorial/
 @Component({
@@ -16,43 +9,31 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class DropDownComponent {
   @Output() emitFilter = new EventEmitter<string>();
-  @HostListener('document:click', ['$event'])
-  clickout(event: any) {
-    if (this.eRef.nativeElement.contains(event.target)) {
-      console.log('inside');
-    } else {
-      console.log('out');
-    }
-  }
+  myDropdown!: any;
 
-  constructor(private eRef: ElementRef) {}
+  clickEvent$!: Observable<Event>;
 
-  form = new FormGroup({
-    filters: new FormControl('', Validators.required),
-  });
+  constructor() {}
 
-  getFormValue() {
-    console.log('pop');
-  }
-
-  setValue(value: any) {
-    this.emitFilter.emit(value);
+  ngOnInit(): void {
+    this.myDropdown = document.getElementById('myDropdown');
   }
 
   operDropdown() {
-    document.getElementById('myDropdown')?.classList.toggle('show');
+    this.myDropdown?.classList.toggle('show');
   }
 
-  // window.onclick = function(event) {
-  //   if (!event.target.matches('.dropbtn')) {
-  //     var dropdowns = document.getElementsByClassName("dropdown-content");
-  //     var i;
-  //     for (i = 0; i < dropdowns.length; i++) {
-  //       var openDropdown = dropdowns[i];
-  //       if (openDropdown.classList.contains('show')) {
-  //         openDropdown.classList.remove('show');
-  //       }
-  //     }
-  //   }
-  // }
+  emitEvent(event: string) {
+    this.emitFilter.emit(event);
+  }
+
+  filterPopularity() {
+    this.emitEvent('popularity');
+    this.myDropdown?.classList.toggle('show');
+  }
+
+  filterTitle() {
+    this.emitEvent('title');
+    this.myDropdown?.classList.toggle('show');
+  }
 }
